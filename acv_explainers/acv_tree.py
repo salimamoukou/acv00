d1f7ed88b2eebc02f3e570e26ae0377bcc9bcdb8
salimamoukou <salimamoukou@gmail.com>
2021-04-27 01:28:34 +0200

    shap_values_acv fast minus ram

--- acv_explainers/acv_tree.py
+++ acv_explainers/acv_tree.py
@@ -15,7 +15,7 @@ class ACVTree(BaseTree):
 
     def shap_values_acv(self, X, S_star, N_star, C=[[]], num_threads=10):
         return cyext_acv.shap_values_acv_leaves(np.array(X, dtype=np.float), self.data, self.values, self.partition_leaves_trees,
-                                       self.leaf_idx_trees, self.leaves_nb, self.scalings,
+                                       self.leaf_idx_trees, self.leaves_nb, self.max_var,
                                        self.node_idx_trees, S_star, N_star, C, num_threads)
 
     def shap_values_acv_adap(self, X, S_star, N_star, size, C=[[]], num_threads=10):
